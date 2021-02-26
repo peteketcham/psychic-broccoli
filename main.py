94cd4881c97335be357c82db5eb69ff281095464
Pete Ketcham <peokuk@gmail.com>
2021-02-25 19:57:55 -0600

    added a lot, successful on 1 of 2 intial cases

--- main.py
+++ main.py
@@ -1,16 +1,138 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+main.py: Barren Land Analysis program
+See README.md for details.
 
+Args:
+    none: use stdin to enter barren rectangles
+Returns:
+    All the fertile land area in square meters, sorted from smallest area to greatest, separated by a space.
+Raises:
+    SyntaxError when input data is invalid
 """
 
+import queue
+import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk
 
-def parse_input(std_in):
-    pass
+
+def parse_input():
+    """
+    Take in a string and return a list of pairs of coordinates
+
+    >>> '{"0 292 399 307"}'
+    [[0, 292, 399, 307]]
+    >>> '{"48 192 351 207", "48 392 351 407", "120 52 135 547", "260 52 275 547"}'
+    [[48, 192, 351, 207],
+     [48, 392, 351, 407],
+     [120, 52, 135, 547],
+     [260, 52, 275, 547]]
+    """
+    lines = []
+    while True:
+        try:
+            line = input()
+            lines.append(line)
+        except EOFError:
+            break
+    line = '\n'.join(lines)
+
+    # TODO: test/handle multiline input
+    # TODO: test/handle multiple case input
+    # TODO: better error handling
+    line = line.strip()
+    line = line.strip('{}')
+    line = line.split(',')
+    line = [pairs.strip().strip('""') for pairs in line]
+    line = [pairs.split() for pairs in line]
+    line = [[int(x) for x in pairs] for pairs in line]
+    return line
+
+
+def mark_barren(field, barren_rectangles):
+    """
+    Mark off the set of rectangles that contain the barren land from the field and return the altered field
+    """
+    for rectangle in barren_rectangles:
+        # mnemonic assignment
+        x_0, y_0, x_1, y_1 = rectangle
+        for x in range(x_0, x_1 + 1):
+            for y in range(y_0, y_1 + 1):
+                field[x][y] = False
+    return field
+
+
+def flood_fill(field, coordinates):
+    """
+    Flood fill 4-ways with queue (https://en.wikipedia.org/wiki/Flood_fill)
+    Args: boolean field (2D array) with barren marked, coordinates of a place to fill
+    Returns: field with flooded area removed, size of area removed
+    """
+    if not field[coordinates[0]][coordinates[1]]:
+        return field, 0
+    q = queue.Queue()
+    q.put(coordinates)
+    count = 0
+    while not q.empty():
+        n = q.get()
+        if field[n[0]][n[1]]:
+            count += 1
+            field[n[0]][n[1]] = False
+            if n[0] >= 0:
+                q.put((n[0] - 1, n[1]))  # up
+            try:
+                q.put((n[0] + 1, n[1]))  # down
+            except IndexError:
+                pass
+            if n[1] >= 0:
+                q.put((n[0], n[1] - 1))  # left
+            try:
+                q.put((n[0], n[1] + 1))  # right
+            except IndexError:
+                pass
+    return field, count
+
+
+def draw_field(field, field_size):
+    """
+    adapted from: https://stackoverflow.com/questions/53308708/how-to-display-an-image-from-a-numpy-array-in-tkinter
+    """
+    root = tk.Tk()
+    array = np.ones(field_size) * 200 * field
+    img = ImageTk.PhotoImage(image=Image.fromarray(array))
+    canvas = tk.Canvas(root, height=field_size[0], width=field_size[1])
+    canvas.pack()
+    canvas.create_image(0, 0, anchor="nw", image=img)
+    root.mainloop()
 
 
 def main():
-    pass
+    """
+
+    """
+    # Assume bounds start at 0,0
+    # field_bounds = [0, 0, 399, 599]
+    field_size = (400, 600)
+    field = np.ones(field_size, dtype=bool)
+    barren_rectangles = parse_input()
+    field = mark_barren(field, barren_rectangles)
+
+    # assorted tests
+    # marked land
+    # print(field)
+    # arable land
+    print(np.sum(field))
+    total = []
+    field, subtotal = flood_fill(field, (0, 0))
+    total.append(subtotal)
+    field, subtotal = flood_fill(field, (0, 580))
+    total.append(subtotal)
+    print(sorted(total), sum(total))
+    
+    # draw the field
+    # draw_field(field, field_size)
 
 
 if __name__ == "__main__":
